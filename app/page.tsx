4451d8dce5f473be78137fde541e2dba49ed6054
testboy123234 <148220701+testboy123234@users.noreply.github.com>
2024-01-30 04:47:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <div className="max-w-5xl mx-auto py-20">
-      <h1 className="text-7xl font-extrabold">Hello, I'm
+      <h1 className="text-7xl font-extrabold">Hello Im
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Pubudu</span>!
       </h1>
       <p className="mt-3 text-xl text-gray-600">Aloha everyone! Checkout my projects!</p>
