3e75259fdac644db10f94330f35487807a3dd6b4
dp lakmal <pubudu.lakamlc@gmail.com>
2024-01-30 03:46:23 +0530

    style frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,39 @@
 import { getProjects } from "@/sanity/schemas/sanity-utils";
+import Image from "next/image";
 
 export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div>
+    <div className="max-w-5xl mx-auto py-20">
+      <h1 className="text-7xl font-extrabold">Hello, I'm
+        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Pubudu</span>!
+      </h1>
+      <p className="mt-3 text-xl text-gray-600">Aloha everyone! Checkout my projects!</p>
+
+      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My projects</h2>
+
+<div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
       {projects.map((project) => (
-        <div key={project._id}>
-          {project.name}
+        <div key={project._id} className="border-2 border-gray-500 rounded-lg p-1">
+
+          {project.image && (
+            <Image src={project.image}
+              alt={project.name}
+              width={750}
+              height={300}
+              className="object-cover rounded-lg border border-gray-500" />
+          )}
+
+          <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+
+            {project.name}
+          </div>
         </div>
       ))}
     </div>
+</div>
   );
 
-  
 }
--- sanity/schemas/sanity-utils.ts
+++ sanity/schemas/sanity-utils.ts
@@ -2,29 +2,29 @@ import { Project } from "@/app/types/Projects";
 import { createClient, groq } from "next-sanity"
 
 export async function getProjects(): Promise<Project[]> {
+    try {
+        const client = createClient({
 
-    const client = createClient({
+            projectId: 'nj8kgzsc',
+            dataset: 'production',
 
-        projectId: 'nj8kgzsc',
-        dataset: 'production',
+            apiVersion: "2024-01-29",
+            useCdn: false,
+        });
 
-        apiVersion: "2024-01-29",
-        useCdn: false,
-    });
-
-    return client.fetch(
-        groq`*[_type == 'project']{
-
-            _id,
-            _createdAt,
-            name,
-            "slug": slug.current,
-            "image": image.asset -> url,
-            url,
-            content
-
-        }`
-    )
-
-
+        return client.fetch(
+            groq`*[_type == "project"]{
+                    _id,
+                    _createdAt,
+                    name,
+                    "slug": slug.current,
+                    "image": image.asset->url,
+                    url,
+                    content
+                }
+            `);
+    } catch (error) {
+        console.error("Error fetching projects:", error);
+        return []; // Return an empty array or handle the error as needed
+    }
 }
